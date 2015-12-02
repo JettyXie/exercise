ee7327d73f67ea3e70570123e57f54d731e09bc1
xietongjian <583110978@qq.com>
2015-12-02 15:53:28 +0800

    create HomeService

---
+++ exercise-core/src/main/java/com/ying/edu/exercise/service/home/IHomeService.java
@@ -0,0 +1,5 @@
+package com.ying.edu.exercise.service.home;
+
+public interface IHomeService {
+
+}
---
+++ exercise-core/src/main/java/com/ying/edu/exercise/service/home/impl/HomeServiceImpl.java
@@ -0,0 +1,7 @@
+package com.ying.edu.exercise.service.home.impl;
+
+import com.ying.edu.exercise.service.home.IHomeService;
+
+public class HomeServiceImpl implements IHomeService {
+
+}
